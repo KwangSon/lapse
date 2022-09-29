5d68e560d264923397475de2e5374ca84b5bb8c6
Kwanghoon Son <kwangson@yahoo.com>
2022-09-29 18:01:29 +0900

    Update stdin, out
    Signed-off-by: Kwanghoon Son <kwangson@yahoo.com>

--- test.c
+++ test.c
@@ -1,24 +1,32 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 const uint LED_PIN = 25;
 
-int main() {
+int main()
+{
 
-bi_decl(bi_program_description("This is a test binary."));
-bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+  bi_decl(bi_program_description("This is a test binary."));
+  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
-stdio_init_all();
+  stdio_init_all();
 
-gpio_init(LED_PIN);
-gpio_set_dir(LED_PIN, GPIO_OUT);
-while (1) {
-gpio_put(LED_PIN, 0);
-sleep_ms(250);
-gpio_put(LED_PIN, 1);
-puts("Hello World\n");
-sleep_ms(1000);
-}
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  while (1)
+  {
+    gpio_put(LED_PIN, 0);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 1);
+    printf("wOrks?\n");
+    printf("waiting input");
+    char x = getchar_timeout_us(10000000);
+    printf("end waiting input");
+    printf("%c", x);
+
+    sleep_ms(1000);
+  }
 }
